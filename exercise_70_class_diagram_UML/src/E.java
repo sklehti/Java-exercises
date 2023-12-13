7faabba353b2fef6c29a6fc125724a65750160c4
Saara Lehtinen <Saara.Lehtinen@edu.karelia.fi>
2023-12-13 08:32:23 +0200

    exercise added

---
+++ exercise_70_class_diagram_UML/src/E.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public class E extends C {
+    private ArrayList<C> list;
+}
---
+++ exercise_70_class_diagram_UML/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * More info about class diagram (UML): <a href="https://ohjelmointi-20.mooc.fi/osa-11/1-luokkakaaviot">...</a>
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
